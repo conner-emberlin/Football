c12e333965f50db716fb4f7b126508f046f1adad
ctemberlin <ctemberlin@gmail.com>
2023-08-03 13:54:56 -0500

    service helper

---
+++ Football.Api/Helpers/ServiceHelper.cs
@@ -0,0 +1,25 @@
+﻿using CsvHelper;
+using Microsoft.AspNetCore.Server.IIS.Core;
+
+namespace Football.Api.Helpers
+{
+    public class ServiceHelper
+    {
+        public string TransformPosition(int pos)
+        {
+            switch (pos)
+            {
+                case 1:
+                    return "QB";
+                    break;
+                case 2:
+                    return "RB";
+                    break;
+                case 3:
+                    return "WR/TE";
+                    break;
+                default: return "QB";
+            }
+        }
+    }
+}
