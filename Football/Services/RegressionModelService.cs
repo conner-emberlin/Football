da96f8dce8c9bc3724287b40d0f212ad1c81f5fd
ctemberlin <ctemberlin@gmail.com>
2023-08-20 10:08:37 -0500

    n

--- Football/Services/RegressionModelService.cs
+++ Football/Services/RegressionModelService.cs
@@ -7,6 +7,7 @@ namespace Football.Services
     {
         private readonly IFantasyService _fantasyService;
         private readonly IPlayerService _playerService;
+        private readonly int currentSeason = 2023;
         public RegressionModelService(IFantasyService fantasyService, IPlayerService playerService)
         {
             _fantasyService = fantasyService;
@@ -69,7 +70,57 @@ namespace Football.Services
                 TouchdownsPerGame = data ? Math.Round((double)(receivingStat.Touchdowns / receivingStat.Games), 4) : 0
             };
         }
+        public RegressionModelQB PopulateProjectedAverageModelQB(PassingStatistic passingStat, RushingStatistic rushingStat, int playerId)
+        {
+            var dataP = passingStat != null;
+            var dataR = passingStat != null;
+            return new RegressionModelQB
+            {
+                PlayerId = playerId,
+                Season = currentSeason,
+                PassingAttemptsPerGame = dataP ? Math.Round((double)(passingStat.Attempts / passingStat.Games), 4) : 0,
+                PassingYardsPerGame = dataP ? Math.Round((double)(passingStat.Yards / passingStat.Games), 4) : 0,
+                PassingTouchdownsPerGame = dataP ? Math.Round((double)(passingStat.Touchdowns / passingStat.Games), 4) : 0,
+                RushingAttemptsPerGame = dataR ? Math.Round((double)(rushingStat.RushAttempts / rushingStat.Games), 4) : 0,
+                RushingYardsPerGame = dataR ? Math.Round((double)(rushingStat.Yards / rushingStat.Games), 4) : 0,
+                RushingTouchdownsPerGame = dataR ? Math.Round((double)(rushingStat.Touchdowns / rushingStat.Games), 4) : 0,
+                SackYardsPerGame = dataP ? Math.Round((double)(passingStat.SackYards / passingStat.Games), 4) : 0
+            };
+        }
 
+        public RegressionModelRB PopulateProjectedAverageModelRB(RushingStatistic rushingStat, ReceivingStatistic receivingStat, int playerId)
+        {
+            var dataRush = rushingStat != null;
+            var dataRec = receivingStat != null;
+            return new RegressionModelRB
+            {
+                PlayerId = playerId,
+                Season = currentSeason,
+                Age = dataRush ? rushingStat.Age : 0,
+                RushingAttemptsPerGame = dataRush ? Math.Round((double)(rushingStat.RushAttempts / rushingStat.Games), 4) : 0,
+                RushingYardsPerGame = dataRush ? Math.Round((double)(rushingStat.Yards / rushingStat.Games), 4) : 0,
+                RushingYardsPerAttempt = dataRush ? Math.Round((double)(rushingStat.Yards / rushingStat.RushAttempts), 4) : 0,
+                RushingTouchdownsPerGame = dataRush ? Math.Round((double)(rushingStat.Touchdowns / rushingStat.Games), 4) : 0,
+                ReceivingTouchdownsPerGame = dataRec ? Math.Round((double)(receivingStat.Touchdowns / receivingStat.Games), 4) : 0,
+                ReceivingYardsPerGame = dataRec ? Math.Round((double)(receivingStat.Yards / receivingStat.Games), 4) : 0,
+                ReceptionsPerGame = dataRec ? Math.Round((double)(receivingStat.Receptions / receivingStat.Games), 4) : 0
+            };
+        }
+
+        public RegressionModelPassCatchers PopulateProjectedAverageModelPassCatchers(ReceivingStatistic receivingStat, int playerId)
+        {
+            var data = receivingStat != null;
+            return new RegressionModelPassCatchers
+            {
+                PlayerId = playerId,
+                Season = currentSeason,
+                TargetsPerGame = data ? Math.Round((double)(receivingStat.Targets / receivingStat.Games), 4) : 0,
+                ReceptionsPerGame = data ? Math.Round((double)(receivingStat.Receptions / receivingStat.Games), 4) : 0,
+                YardsPerGame = data ? Math.Round((double)(receivingStat.Yards / receivingStat.Games), 4) : 0,
+                YardsPerReception = data ? Math.Round((double)receivingStat.Yards / receivingStat.Receptions, 4) : 0,
+                TouchdownsPerGame = data ? Math.Round((double)(receivingStat.Touchdowns / receivingStat.Games), 4) : 0
+            };
+        }
         public async Task<List<FantasyPoints>> PopulateFantasyResults(int season, string position)
         {
             List<FantasyPoints> fantasyPoints = new();
