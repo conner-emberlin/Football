140d6dde182e1b548c75e7d40fe3bdbb1e337b64
ctemberlin <ctemberlin@gmail.com>
2023-08-21 13:41:44 -0500

    c

--- Football.News/Program.cs
+++ Football.News/Program.cs
@@ -7,12 +7,6 @@ namespace News
     {
         public static async Task<int> Main()
         {
-            var news = new NewsService();
-            var root = await news.GetEspnNews();
-            foreach(var a in root.articles)
-            {
-                Console.WriteLine(a.headline);
-            }
             return 1;
         }
     }
