cd9871aed297f8c4bfaf2a292bb6bc6c44f4cf31
ctemberlin <ctemberlin@gmail.com>
2023-11-23 11:15:04 -0600

    position colors

---
+++ Football.UI/Helpers/Formatter.cs
@@ -0,0 +1,19 @@
+﻿namespace Football.UI.Helpers
+{
+    public class Formatter
+    {
+        public string PositionColor(string position)
+        {
+            return position switch
+            {
+                "QB" => "#ff2a6d",
+                "RB" => "#00ceb8",
+                "WR" => "#58a7ff",
+                "TE" => "#ffae58",
+                "DST" => "#7988a1",
+                _ => "#000000"
+            };
+        }
+        public int Rank<T>(List<T> list, T item) => list.IndexOf(item) + 1;
+    }
+}
