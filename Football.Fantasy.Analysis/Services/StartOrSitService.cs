faebef52b3315a25c755257bdd3b279070e879c4
ctemberlin <ctemberlin@gmail.com>
2023-12-14 19:15:02 -0600

    bugs

--- Football.Fantasy.Analysis/Services/StartOrSitService.cs
+++ Football.Fantasy.Analysis/Services/StartOrSitService.cs
@@ -127,7 +127,7 @@ namespace Football.Fantasy.Analysis.Services
                 if (schedule != null && playerProjection > 0)
                 {       
                     var opponentId = schedule.HomeTeamId == teamId ? schedule.AwayTeamId : schedule.HomeTeamId;
-                    var opponentSchedule = (await playersService.GetTeamGames(opponentId)).Where(s => s.Week < currentWeek && s.OpposingTeam != "BYE");
+                    var opponentSchedule = (await playersService.GetTeamGames(opponentId)).Where(s => s.Week < currentWeek && s.OpposingTeamId > 0);
                     foreach (var match in opponentSchedule)
                     {
                         var otherPlayers = await playersService.GetPlayersByTeam(match.OpposingTeam);
