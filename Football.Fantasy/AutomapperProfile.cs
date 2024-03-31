d608ebd3bc5bf534892a5ad4b762b85ef0abeae4
ctemberlin <ctemberlin@gmail.com>
2024-03-31 18:35:18 -0500

    weekly fantasy automap

--- Football.Fantasy/AutomapperProfile.cs
+++ Football.Fantasy/AutomapperProfile.cs
@@ -19,14 +19,32 @@ namespace Football.Fantasy
             CreateMap<SeasonDataTE, SeasonFantasy>()
                      .ForMember(sf => sf.Position, o => o.MapFrom(sf => Enums.Position.TE.ToString()))
                      .ForMember(sf => sf.FantasyPoints, o => o.Ignore());
-            CreateMap<SeasonDataQB, SeasonFantasy>()
-                     .ForMember(sf => sf.Position, o => o.MapFrom(sf => Enums.Position.DST.ToString()))
+
+
+            CreateMap<WeeklyDataQB, WeeklyFantasy>()
+                     .ForMember(sf => sf.Position, o => o.MapFrom(sf => Enums.Position.QB.ToString()))
+                     .ForMember(sf => sf.Games, o => o.MapFrom(sf => 1))
                      .ForMember(sf => sf.FantasyPoints, o => o.Ignore());
-            CreateMap<SeasonDataQB, SeasonFantasy>()
+            CreateMap<WeeklyDataRB, WeeklyFantasy>()
+                     .ForMember(sf => sf.Position, o => o.MapFrom(sf => Enums.Position.RB.ToString()))
+                     .ForMember(sf => sf.Games, o => o.MapFrom(sf => 1))
+                     .ForMember(sf => sf.FantasyPoints, o => o.Ignore());
+            CreateMap<WeeklyDataWR, WeeklyFantasy>()
+                     .ForMember(sf => sf.Position, o => o.MapFrom(sf => Enums.Position.WR.ToString()))
+                     .ForMember(sf => sf.Games, o => o.MapFrom(sf => 1))
+                     .ForMember(sf => sf.FantasyPoints, o => o.Ignore());
+            CreateMap<WeeklyDataTE, WeeklyFantasy>()
+                     .ForMember(sf => sf.Position, o => o.MapFrom(sf => Enums.Position.TE.ToString()))
+                     .ForMember(sf => sf.Games, o => o.MapFrom(sf => 1))
+                     .ForMember(sf => sf.FantasyPoints, o => o.Ignore());
+            CreateMap<WeeklyDataQB, WeeklyFantasy>()
+                     .ForMember(sf => sf.Position, o => o.MapFrom(sf => Enums.Position.DST.ToString()))
+                     .ForMember(sf => sf.Games, o => o.MapFrom(sf => 1))
+                     .ForMember(sf => sf.FantasyPoints, o => o.Ignore());
+            CreateMap<WeeklyDataQB, WeeklyFantasy>()
                     .ForMember(sf => sf.Position, o => o.MapFrom(sf => Enums.Position.K.ToString()))
+                    .ForMember(sf => sf.Games, o => o.MapFrom(sf => 1))
                     .ForMember(sf => sf.FantasyPoints, o => o.Ignore());
-
-
         }
     }
 }
--- Football.Fantasy/Services/FantasyCalculator.cs
+++ Football.Fantasy/Services/FantasyCalculator.cs
@@ -3,7 +3,6 @@ using Football.Data.Models;
 using Football.Fantasy.Interfaces;
 using Football.Fantasy.Models;
 using Football.Models;
-using Football.Enums;
 using Microsoft.Extensions.Options;
 
 namespace Football.Fantasy.Services
@@ -22,19 +21,11 @@ namespace Football.Fantasy.Services
         }
         public WeeklyFantasy CalculateFantasy(WeeklyDataQB stat)
         {
-            var points = _scoring.PointsPerPassingTouchdown * stat.TD + _scoring.PointsPerTouchdown * stat.RushingTD
+            var weeklyFantasy = mapper.Map<WeeklyFantasy>(stat);
+            weeklyFantasy.FantasyPoints = _scoring.PointsPerPassingTouchdown * stat.TD + _scoring.PointsPerTouchdown * stat.RushingTD
                         + _scoring.PointsPerPassingYard * stat.Yards + _scoring.PointsPerYard * stat.RushingYards
                         - _scoring.PointsPerFumble * stat.Fumbles - _scoring.PointsPerInterception * stat.Int;
-            return new WeeklyFantasy
-            {
-                PlayerId = stat.PlayerId,
-                Season = stat.Season,
-                Week = stat.Week,
-                Games = 1,
-                FantasyPoints = points,
-                Name = stat.Name,
-                Position = Position.QB.ToString()
-            };
+            return weeklyFantasy;
         }
         public SeasonFantasy CalculateFantasy(SeasonDataRB stat)
         {
@@ -46,19 +37,11 @@ namespace Football.Fantasy.Services
         }
         public WeeklyFantasy CalculateFantasy(WeeklyDataRB stat)
         {
-            var points = _scoring.PointsPerYard * stat.RushingYds + _scoring.PointsPerTouchdown * stat.RushingTD
+            var weeklyFantasy = mapper.Map<WeeklyFantasy>(stat);
+            weeklyFantasy.FantasyPoints = _scoring.PointsPerYard * stat.RushingYds + _scoring.PointsPerTouchdown * stat.RushingTD
                         + _scoring.PointsPerReception * stat.Receptions + _scoring.PointsPerTouchdown * stat.ReceivingTD
                         + _scoring.PointsPerYard * stat.Yards - _scoring.PointsPerFumble * stat.Fumbles;
-            return new WeeklyFantasy
-            {
-                PlayerId = stat.PlayerId,
-                Season = stat.Season,
-                Week = stat.Week,
-                Games = 1,
-                FantasyPoints = points,
-                Name = stat.Name,
-                Position = Position.RB.ToString()
-            };
+            return weeklyFantasy;
         }
         public SeasonFantasy CalculateFantasy(SeasonDataWR stat)
         {
@@ -70,19 +53,11 @@ namespace Football.Fantasy.Services
         }
         public WeeklyFantasy CalculateFantasy(WeeklyDataWR stat)
         {
-            var points = _scoring.PointsPerReception * stat.Receptions + _scoring.PointsPerYard * stat.Yards
+            var weeklyFantasy = mapper.Map<WeeklyFantasy>(stat);
+            weeklyFantasy.FantasyPoints = _scoring.PointsPerReception * stat.Receptions + _scoring.PointsPerYard * stat.Yards
                         + _scoring.PointsPerTouchdown * stat.TD + _scoring.PointsPerYard * stat.RushingYds
                         + _scoring.PointsPerTouchdown * stat.RushingTD - _scoring.PointsPerFumble * stat.Fumbles;
-            return new WeeklyFantasy
-            {
-                PlayerId = stat.PlayerId,
-                Season = stat.Season,
-                Week = stat.Week,
-                Games = 1,
-                FantasyPoints = points,
-                Name = stat.Name,
-                Position = Position.WR.ToString()
-            };
+            return weeklyFantasy;
         }
         public SeasonFantasy CalculateFantasy(SeasonDataTE stat)
         {
@@ -94,22 +69,15 @@ namespace Football.Fantasy.Services
         }
         public WeeklyFantasy CalculateFantasy(WeeklyDataTE stat)
         {
-            var points = _scoring.PointsPerReception * stat.Receptions + _scoring.PointsPerYard * stat.Yards
+            var weeklyFantasy = mapper.Map<WeeklyFantasy>(stat);
+            weeklyFantasy.FantasyPoints = _scoring.PointsPerReception * stat.Receptions + _scoring.PointsPerYard * stat.Yards
                         + _scoring.PointsPerTouchdown * stat.TD + _scoring.PointsPerYard * stat.RushingYds
                         + _scoring.PointsPerTouchdown * stat.RushingTD - _scoring.PointsPerFumble * stat.Fumbles;
-            return new WeeklyFantasy
-            {
-                PlayerId = stat.PlayerId,
-                Season = stat.Season,
-                Week = stat.Week,
-                Games = 1,
-                FantasyPoints = points,
-                Name = stat.Name,
-                Position = Position.TE.ToString()
-            };
+            return weeklyFantasy;
         }
         public WeeklyFantasy CalculateFantasy(WeeklyDataDST stat, WeeklyDataDST opponentStat, GameResult result, int teamId)
         {
+            var weeklyFantasy = mapper.Map<WeeklyFantasy>(stat);
             var pointsAllowed = result.WinnerId == teamId ? result.LoserPoints - _scoring.PointsPerTouchdown * opponentStat.DefensiveTD 
                                 : result.WinnerPoints - _scoring.PointsPerTouchdown * opponentStat.DefensiveTD;
 
@@ -122,44 +90,26 @@ namespace Football.Fantasy.Services
                                      : 35 <= pointsAllowed && pointsAllowed <= 45 ? _scoring.ThirtyFiveToFourtyFivePointsAllowed
                                      : _scoring.FourtySixOrMorePointsAllowed;
 
-            var fantasyPoints = pointsAllowedFantasy
-                                + stat.Sacks * _scoring.PointsPerSack
-                                + stat.Ints * _scoring.PointsPerInterception
-                                + stat.FumblesRecovered * _scoring.PointsPerFumble
-                                + (stat.SpecialTD + stat.DefensiveTD) * _scoring.PointsPerTouchdown
-                                + stat.Safties * _scoring.PointsPerSafety;
+            weeklyFantasy.FantasyPoints = pointsAllowedFantasy
+                                        + stat.Sacks * _scoring.PointsPerSack
+                                        + stat.Ints * _scoring.PointsPerInterception
+                                        + stat.FumblesRecovered * _scoring.PointsPerFumble
+                                        + (stat.SpecialTD + stat.DefensiveTD) * _scoring.PointsPerTouchdown
+                                        + stat.Safties * _scoring.PointsPerSafety;
 
-            return new WeeklyFantasy
-            {
-                PlayerId = stat.PlayerId,
-                Season = stat.Season,
-                Week = stat.Week,
-                Games = stat.Games,
-                FantasyPoints = fantasyPoints,
-                Name = stat.Name,
-                Position = Position.DST.ToString()
-            };
+            return weeklyFantasy;
         }
 
         public WeeklyFantasy CalculateFantasy(WeeklyDataK stat)
         {
-            var points = stat.ExtraPoints * _scoring.ExtraPoint
+            var weeklyFantasy = mapper.Map<WeeklyFantasy>(stat);
+            weeklyFantasy.FantasyPoints = stat.ExtraPoints * _scoring.ExtraPoint
                          + (stat.ExtraPointAttempts - stat.ExtraPoints) * _scoring.ExtraPointMissed
                          + (stat.FieldGoalAttempts - stat.FieldGoals) * _scoring.FGMissed
                          + (stat.OneNineteen + stat.TwentyTwentyNine + stat.ThirtyThirtyNine) * _scoring.FGLessThanFourty
                          + stat.FourtyFourtyNine * _scoring.FGFourtyFifty
                          + stat.Fifty * _scoring.FGGreaterThanFifty;
-
-            return new WeeklyFantasy
-            {
-                PlayerId = stat.PlayerId,
-                Season = stat.Season,
-                Week = stat.Week,
-                Games = stat.Games,
-                FantasyPoints = points,
-                Name = stat.Name,
-                Position = Position.K.ToString()
-            };
+            return weeklyFantasy;
         }
 
 
