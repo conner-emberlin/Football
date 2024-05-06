d966587c683440dd6c1caba71b9abcebbfc02182
ctemberlin <ctemberlin@gmail.com>
2024-05-05 20:40:08 -0500

    tests

--- Football.Tests/StatisticsServiceShould.cs
+++ Football.Tests/StatisticsServiceShould.cs
@@ -19,6 +19,9 @@ namespace Football.Tests
 
         private readonly int _playerId = 1;
         private readonly string _team = "TM";
+        private readonly int _season = 2023;
+        private readonly int _teamId = 10;
+        private readonly TeamMap _teamMap;
 
         private readonly Mock<IStatisticsRepository> _mockStatisticsRepository;
         private readonly Mock<ISettingsService> _mockSettingsService;
@@ -30,6 +33,11 @@ namespace Football.Tests
             _mockStatisticsRepository = _mock.GetMock<IStatisticsRepository>();
             _mockSettingsService = _mock.GetMock<ISettingsService>();
             _mockPlayersService = _mock.GetMock<IPlayersService>();
+
+            _teamMap = new() { PlayerId = _playerId, Team = _team, TeamDescription = "Description", TeamId = _teamId };
+
+            _mockPlayersService.Setup(ps => ps.GetCurrentWeek(_season)).ReturnsAsync(1);
+            _mockPlayersService.Setup(ps => ps.GetAllTeams()).ReturnsAsync([_teamMap]);
 
             _sut = new StatisticsService(_mockStatisticsRepository.Object, _mockPlayersService.Object, _mockSettingsService.Object);
         }
@@ -54,6 +62,36 @@ namespace Football.Tests
             var actual = await _sut.GetWeeklyData<WeeklyDataQB>(Position.QB, _playerId, _team);
             var observedMinWeek = actual.Min(a => a.Week);
             Assert.Equal(2, observedMinWeek);
+        }
+
+        [Fact]
+        public async Task GetTeamRecords_NoGameResults_ReturnsZeros()
+        {
+            _mockStatisticsRepository.Setup(sr => sr.GetGameResults(_season)).ReturnsAsync([]);
+            
+            var actual = await _sut.GetTeamRecords(_season);
+            var observed = actual.FirstOrDefault();
+
+            Assert.NotNull(observed);
+            Assert.Equal(0, observed.Wins);
+            Assert.Equal(0, observed.Losses);
+            Assert.Equal(0, observed.Ties);
+        }
+
+        [Fact]
+        public async Task GetTeamRecords_CalculatesTies()
+        {
+            var gameResult = new GameResult { HomeTeamId = _teamId, AwayTeamId = 2, LoserPoints = 10, WinnerPoints = 10, Season = _season };
+
+            _mockStatisticsRepository.Setup(sr => sr.GetGameResults(_season)).ReturnsAsync([gameResult]);
+
+            var actual = await _sut.GetTeamRecords(_season);
+            var observed = actual.FirstOrDefault();
+
+            Assert.NotNull(observed);
+            Assert.Equal(0, observed.Wins);
+            Assert.Equal(0, observed.Losses);
+            Assert.Equal(1, observed.Ties);
         }
     }
 }
