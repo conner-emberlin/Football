4bdb3ea42aa6093f24fa15967fa15819089a3458
ctemberlin <ctemberlin@gmail.com>
2024-07-14 16:34:41 -0500

    settings

--- Football.Api/Program.cs
+++ Football.Api/Program.cs
@@ -83,7 +83,6 @@ builder.Services.Configure<ReplacementLevels>(builder.Configuration.GetSection("
 builder.Services.Configure<FantasyScoring>(builder.Configuration.GetSection("FantasyScoring"));
 builder.Services.Configure<ProjectionLimits>(builder.Configuration.GetSection("Projections"));
 builder.Services.Configure<Starters>(builder.Configuration.GetSection("Starters"));
-builder.Services.Configure<Tunings>(builder.Configuration.GetSection("Tunings"));
 builder.Services.Configure<WeeklyTunings>(builder.Configuration.GetSection("WeeklyTunings"));
 builder.Services.Configure<WeeklyScraping>(builder.Configuration.GetSection("Scraping"));
 builder.Services.Configure<MockDraftSettings>(builder.Configuration.GetSection("MockDraftSettings"));
