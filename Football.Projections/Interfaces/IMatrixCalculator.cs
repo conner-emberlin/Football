2f87537afc5df5deb4257ddb243a7c3412449c4d
ctemberlin <ctemberlin@gmail.com>
2024-11-22 19:49:23 -0600

    bad copy

--- Football.Projections/Interfaces/IMatrixCalculator.cs
+++ Football.Projections/Interfaces/IMatrixCalculator.cs
@@ -8,7 +8,7 @@ namespace Football.Projections.Interfaces
         Matrix<double> RegressorMatrix<T>(List<T> model, List<string>? filter = null);
         Vector<double> DependentVector<T>(List<T> dependents, Model value);
         Vector<double> TransformModel<T>(T modelItem, List<string>? filter = null);
-        Vector<double> ReLU(Vector<double> x) => x.Map(value => Math.Max(0, value));
-        Vector<double> ReLUDerivative(Vector<double> x) => x.Map(value => value > 0.0 ? 1.0 : 0.0);
+        Vector<double> ReLU(Vector<double> x);
+        Vector<double> ReLUDerivative(Vector<double> x);
     }
 }
